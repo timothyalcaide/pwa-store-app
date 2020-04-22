3632ea5f8f68842caad33e1bb9fac058c3e97338
Timothy Alcaide <timothyalcaide@gmail.com>
2020-04-22 16:45:17 +0200

    Project initialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  template: `
+    <h1>Store Inline Template</h1>
+    <router-outlet></router-outlet>
+  `,
+  styles: [
+    `
+      h1 {
+        color: hotpink;
+      }
+    `,
+  ],
 })
-export class AppComponent {
-  title = 'store-app';
-}
+export class AppComponent {}
